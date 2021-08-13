402de3cab7e49deca554dc771ec4794c28b86e55
mimi <maryamlouhabi@gmail.com>
2021-08-13 13:10:11 +0100

    update

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ con.on('open', () => {
 
 app.use(express.json())
 
-
-app.listen(3000, () => {
+const port= process.env.PORT || 3000
+app.listen(port, () => {
     console.log('Server started')
 })
 const bdRouter = require('./routers/bd')
